88a2dc114b5b9f0fb6ae98ec11c851cff99f9fab
Prakash Salapu <prakash.salapu@gmail.com>
2026-05-20 12:10:58 +0530

    feat: create frontend folder structure with base components and utilities

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,11 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import "./App.css";
+import React from 'react';
 
 function App() {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    axios
-      .get(`${import.meta.env.VITE_API_URL}/message`)
-      .then((res) => setMessage(res.data))
-      .catch((err) => {
-        console.error("Error:", err);
-        setMessage("Error connecting to backend ❌");
-      });
-  }, []);
 
   return (
     <div className="App">
-      <h1>MERN Boilerplate</h1>
-      <p>Message from backend: {message}</p>
+      <h1>CareerOS</h1>
+      
     </div>
   );
 }
